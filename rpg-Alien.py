574fede4212074d5b009b3c1b00af7a44e171c21
rick98901p-ctrl9 <rick98901p@gmail.com>
2025-10-21 15:10:00 -0300

    Marcos: classe personagem e a possibilidade de escolher o nome do seu personagem

--- rpg-Alien.py
+++ rpg-Alien.py
@@ -1,1 +1,13 @@
 # Desenvolva o seu jogo aqui
+
+class personagem():
+    def__int__(nome,vida,ataque,defesa)
+    self.name = Name
+    self.class = class
+    self.vida = 100
+    self.forca = 10
+    self.sanidade = 10
+    self.agilidade = 
+    self.definir_atributos
+
+nome = input("escolha um nome: ")
