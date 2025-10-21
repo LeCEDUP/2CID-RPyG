600fa897f0af603f0246e4cf866b8f1398ff47f9
rick98901p-ctrl9 <rick98901p@gmail.com>
2025-10-21 16:22:06 -0300

    Henrique e Marcos: Apagamos o código pois erramos

--- rpg-Alien.py
+++ rpg-Alien.py
@@ -1,13 +1,2 @@
-# Desenvolva o seu jogo aqui
-
-class personagem():
-    def__int__(nome,vida,ataque,defesa)
-    self.name = Name
-    self.class = class
-    self.vida = 100
-    self.forca = 10
-    self.sanidade = 10
-    self.agilidade = 
-    self.definir_atributos
-
-nome = input("escolha um nome: ")
+nome_do_heroi = input("Digite nome do seu heroi:")
+meu_heroi = Heroi(nome_do_heroi, 100, 30, 5)
