b5847e50147a5032704a34ebb99a3f515cc24fc5
Alicite <lunacarter1503@gmail.com>
2025-10-21 15:07:44 -0300

    Le: Cria os prints do menu

--- rpg.py
+++ rpg.py
@@ -1,1 +1,9 @@
 # Desenvolva o seu jogo aqui
+def menu():
+    print('------------------------')
+    print('-----TÍTULO DO JOGO-----')
+    print('------------------------')
+    print('-------INTRODUÇÃO-------')
+    print('-------INTRODUÇÃO-------')
+    print('-------INTRODUÇÃO-------')
+    print('------------------------')
