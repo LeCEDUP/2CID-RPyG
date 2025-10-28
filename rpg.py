92a2afa1c4c015b7b074793f8b21ba2fde716305
anabellyrodrigues2020-eng <anabellyrodrigues2020@gmail.com>
2025-10-28 15:49:34 -0300

    criando a class arma

--- rpg.py
+++ rpg.py
@@ -10,3 +10,7 @@ class Item:
         self.nome = nome
         self.descricao = descricao
 
+class Arma(Item):
+    def __init__(self, nome, descricao, bonus_ataque):
+        super().__init__(nome, descricao)
+        self.bonus_ataque = bonus_ataque
