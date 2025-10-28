cce42f78753a228de81a4946db4a4bc0fc4971a6
rick98901p-ctrl9 <rick98901p@gmail.com>
2025-10-28 09:40:43 -0300

    Henrique: Refazendo o código de forma mais efetiva

--- rpg-Alien.py
+++ rpg-Alien.py
@@ -1,17 +1,29 @@
-from personagens.heroi import Heroi
+from itens.item import Item
 from itens.arma import Arma
+from itens.armadura import Armadura
+from personagens.heroi import Heroi
 from personagens.monstro import Monstro
+import time
 
-def menu():
-    print('---Alien, o 8.º Passageiro pt2---')
-    print('----------------------')
-    print('----------------------')
-    print('----------------------')
-    print('----------------------')
-    print('----------------------')
-    print('----------------------')
+def menu_principal():
+    print("\n=== ALIEN: O 8.º PASSAGEIRO ===")
+    print("1 - Iniciar Missão")
+    print("2 - Sair")
+    escolha = input("Escolha uma opção: ")
+    
+def criar_tripulante():
+    def criar_tripulante():
+        nome = input("\nDigite o nome do tripulante: ")
+        tripulante = heroi(nome, 100, 15,3)
+        print(f"\n{tripulante.nome} acorda da criossonia... algo está errado na nave Nostromo.")
+        return tripulante   
+
+def iniciar_jogo():
     nome_do_heroi = input('digite o nome do seu heroi')
-meu_heroi = heroi(nome_do_heroi, 100, 20, 10)
-rifle_automatico = Arma("Rifle automático F903WE", "um rifle automático.", 10)
-Xenomorfo = Monstro("Xenomorfo", 30, 8, 2, "Um alíenigina ")
-armadura_M4X = Armadura("Escudo de Ferro", "Um escudo resistente.", 5)
+    meu_heroi = heroi(nome_do_heroi, 100, 20, 10)
+
+def evento_item(tripulante, rota):
+    possiveis_itens = [
+        Arma("Rifle automático RMC F903WE", "Uma arma improvisada, eficaz contra o alien.", 12),
+        Item("Kit Médico", "Restaura 40 de vida."),
+        Armadura("Armadura M4X", "Protege contra ataques ácidos e projéteis.", 6)
