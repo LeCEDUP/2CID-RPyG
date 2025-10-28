60b560a685b9d87b06aeffe0624e0b83174fd4fd
thiagowillianh-hub <thiagowillianh@gmail.com>
2025-10-28 15:14:53 -0300

    Iniciando o projeto

--- rpg.py
+++ rpg.py
@@ -1,1 +1,16 @@
-# Desenvolva o seu jogo aqui
+
+
+from itens.item import Item
+from itens.arma import Arma
+from itens.armadura import Armadura
+from personagens.heroi import Heroi
+from personagens.monstro import Monstro
+
+heroi = Heroi("Silas 'Coiote' Graves", 100, 20, 4)
+bandido = Monstro("Bandido da Fronteira", 40, 10, 3, "Humano")
+chefe_bando = Monstro("Chefe dos Abutres", 150, 25, 8, "Humano")    
+
+
+
+
+
