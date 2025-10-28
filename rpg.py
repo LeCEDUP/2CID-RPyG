7b1790ec6a0f3803da796cb0be9fcc2bedb07b07
anabellyrodrigues2020-eng <anabellyrodrigues2020@gmail.com>
2025-10-28 15:52:03 -0300

    criando a class heroi

--- rpg.py
+++ rpg.py
@@ -14,8 +14,17 @@ class Arma(Item):
     def __init__(self, nome, descricao, bonus_ataque):
         super().__init__(nome, descricao)
         self.bonus_ataque = bonus_ataque
-        
+
 class Armadura(Item):
     def __init__(self, nome, descricao, bonus_defesa):
         super().__init__(nome, descricao)
         self.bonus_defesa = bonus_defesa
+
+class Heroi:
+    def __init__(self, nome, vida, ataque, defesa):
+        self.nome = nome
+        self.vida = vida
+        self.ataque = ataque
+        self.defesa = defesa
+        self.inventario = []
+
