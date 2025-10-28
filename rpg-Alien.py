b53b6a7160fd4562295e03bc3bf74f044854f26f
rick98901p-ctrl9 <rick98901p@gmail.com>
2025-10-28 08:07:36 -0300

    Marcos: adição de mais itens e elementos do rpg

--- rpg-Alien.py
+++ rpg-Alien.py
@@ -1,2 +1,17 @@
-nome_do_heroi = input("Digite nome do seu heroi:")
-meu_heroi = Heroi(nome_do_heroi, 100, 30, 5)
+from personagens.heroi import Heroi
+from itens.arma import Arma
+from personagens.monstro import Monstro
+
+def menu():
+    print('---Alien, o 8.º Passageiro pt2---')
+    print('----------------------')
+    print('----------------------')
+    print('----------------------')
+    print('----------------------')
+    print('----------------------')
+    print('----------------------')
+    nome_do_heroi = input('digite o nome do seu heroi')
+meu_heroi = heroi(nome_do_heroi, 100, 20, 10)
+rifle_automatico = Arma("Rifle automático F903WE", "um rifle automático.", 10)
+Xenomorfo = Monstro("Xenomorfo", 30, 8, 2, "Um alíenigina ")
+armadura_M4X = Armadura("Escudo de Ferro", "Um escudo resistente.", 5)
