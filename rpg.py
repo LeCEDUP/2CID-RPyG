b7153d7217f1e1a788fe4561e652db6e89f9a4f2
thiagowillianh-hub <thiagowillianh@gmail.com>
2025-10-28 15:49:47 -0300

    thiago w : Criando itens faroeste

--- rpg.py
+++ rpg.py
@@ -10,7 +10,9 @@ heroi = Heroi("Silas 'Coiote' Graves", 100, 20, 4)
 bandido = Monstro("Bandido da Fronteira", 40, 10, 3, "Humano")
 chefe_bando = Monstro("Chefe dos Abutres", 150, 25, 8, "Humano")    
 
-
+revolver = Arma("Revólver Colt", "Um clássico revólver de seis tiros.", 15)
+colete = Armadura("Colete de Couro", "Um colete resistente de couro curtido.", 6)
+whisky = Item("Garrafa de Whisky", "Recupera 40 pontos de vida.")
 
 
 
