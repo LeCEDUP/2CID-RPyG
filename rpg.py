d13cbbeb48308db72b3402bdb7af5ad34e40790a
eriklubke64 <seu.email@example.com>
2025-11-04 10:14:04 -0300

    importei os itens e personagens

--- rpg.py
+++ rpg.py
@@ -1,2 +1,6 @@
-# Desenvolva o seu jogo aqui
-
+#rpg
+from itens.item import Item
+from itens.arma import Arma
+from itens.armadura import Armadura
+from personagens.heroi import Heroi
+from personagens.monstro import Monstro
