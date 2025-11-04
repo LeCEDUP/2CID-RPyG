535a85631dffe24bc5bcb929f4b71320d269adac
eriklubke64 <seu.email@example.com>
2025-11-04 10:15:17 -0300

    criei alguns personagens

--- rpg.py
+++ rpg.py
@@ -4,3 +4,7 @@ from itens.arma import Arma
 from itens.armadura import Armadura
 from personagens.heroi import Heroi
 from personagens.monstro import Monstro
+
+heroi = Heroi("Lina", 100, 10, 4)
+lobo = Monstro("Lobo Selvagem", 40, 8, 2, "Médio")
+troll = Monstro("Troll das Montanhas", 120, 15, 6, "Grande")
