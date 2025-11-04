b4c449268e4cbffc9837c1779e2b7796e85f0016
thiagowillianh-hub <thiagowillianh@gmail.com>
2025-11-04 10:54:31 -0300

    João Gabriel: Função de usar poção e ver status

--- rpg.py
+++ rpg.py
@@ -38,3 +38,29 @@ def status():
     print(f" Defesa: {heroi.defesa}")
     print(f" Ataque base: {heroi.ataque}")
     print(f" Inventário: {[item.nome for item in heroi.inventario]}\n")
+
+
+for inimigo in inimigos:
+    print(f"\n Um {inimigo.nome} apareceu! Prepare-se!\n")
+
+    while heroi.esta_vivo() and inimigo.esta_vivo():
+        print("\nO que deseja fazer?")
+        print("1 - Atacar")
+        print("2 - Usar Poção")
+        print("3 - Ver Status")
+
+        escolha = input("> ")
+
+        if escolha == "1":
+            heroi.atacar(inimigo)
+            if inimigo.esta_vivo():
+                inimigo.atacar(heroi)
+
+        elif escolha == "2":
+            usar_pocao()
+            if inimigo.esta_vivo():
+                inimigo.atacar(heroi)
+
+        elif escolha == "3":
+            status()
+            continue
