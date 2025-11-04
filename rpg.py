782177dfb9076706588518a0c9f7e390c11a5829
eriklubke64 <seu.email@example.com>
2025-11-04 10:49:38 -0300

    criei a classe item

--- rpg.py
+++ rpg.py
@@ -1,1 +1,8 @@
 #rpg
+class Item:
+    def __init__(self, nome, descricao):
+        self.nome = nome
+        self.descricao = descricao
+
+    def __str__(self):
+        return f"{self.nome}: {self.descricao}"
