4b8db9be581774dc8ccb2cfd1415e81aa7a426a7
thiagowillianh-hub <thiagowillianh@gmail.com>
2025-11-04 10:19:19 -0300

    joao gabriel e thiago: decidimos recomeçar pois estavamos presos no código sem conseguir avançar

--- rpg.py
+++ rpg.py
@@ -1,47 +1,12 @@
-
-
 from itens.item import Item
 from itens.arma import Arma
 from itens.armadura import Armadura
 from personagens.heroi import Heroi
 from personagens.monstro import Monstro
+import random
 
-heroi = Heroi("Silas 'Coiote' Graves", 100, 20, 4)
-bandido = Monstro("Bandido da Fronteira", 40, 10, 3, "Humano")
-chefe_bando = Monstro("Chefe dos Abutres", 150, 25, 8, "Humano")    
+heroi = Heroi("Arthur", 100, 15, 5)
 
-revolver = Arma("Revólver Colt", "Um clássico revólver de seis tiros.", 15)
-colete = Armadura("Colete de Couro", "Um colete resistente de couro curtido.", 6)
-whisky = Item("Garrafa de Whisky", "Recupera 40 pontos de vida.")
-
-print("=== INÍCIO DA AVENTURA NO VELHO OESTE ===\n")
-
-heroi.inventario.append(revolver)
-heroi.inventario.append(colete)
-heroi.inventario.append(whisky)
-print(f"{heroi.nome} encontrou um {revolver.nome}, um {colete.nome} e uma {whisky.nome}.\n")
-
-heroi.equipar_item(revolver)
-heroi.equipar_item(colete)
-
-print("--- DUELO NA FRONTEIRA ---")
-while heroi.esta_vivo() and bandido.esta_vivo():
-    heroi.atacar(bandido)
-    if bandido.esta_vivo():
-        bandido.atacar(heroi)
-
-if heroi.esta_vivo():
-    print(f"{heroi.nome} derrotou o {bandido.nome} no duelo!")
-    heroi.ganhar_experiencia(60)
-    print(f"Vida atual: {heroi.vida}")
-else:
-    print(f"{heroi.nome} foi derrubado pelo {bandido.nome}...") 
-
-print("\n--- PAUSA NO SALOON ---")
-if whisky heroi.inventario:
-    heroi.vida += 40
-    heroi.inventario.remove(whisky)
-    print(f"{heroi.nome} tomou um {whisky.nome} e recuperou energias. vida atual: {heroi.vida}")
-    
-
-
+espada = Arma("Espada Longa", "Uma espada afiada.", 10)
+escudo = Armadura("Escudo de Ferro", "Um escudo resistente.", 5)
+pocao = Item("Poção de Vida", "Restaura 30 de vida.")
