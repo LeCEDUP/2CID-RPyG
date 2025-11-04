96c6974168b62f5bd5bcd10d8bf089a0693caba0
Isadora <isadoragrein34@gmail.com>
2025-11-04 08:07:41 -0300

    função de narração

--- rpg.py
+++ rpg.py
@@ -1,1 +1,9 @@
 # Desenvolva o seu jogo aqui
+import time
+import random
+
+def narrar(texto, atraso=0.04):
+    for caractere in texto:
+        print(caractere, end='', flush=True)
+        time.sleep(atraso)
+    print()
