25d37d0d63a9a79de7d4a58f933e66384adadfa9
eriklubke64 <seu.email@example.com>
2025-11-04 10:51:44 -0300

    classe arma

--- rpg.py
+++ rpg.py
@@ -6,3 +6,8 @@ class Item:
 
     def __str__(self):
         return f"{self.nome}: {self.descricao}"
+    
+    class Arma(Item):
+        def __init__(self, nome, descricao, dano):
+            super().__init__(nome, descricao)
+            self.dano = dano
